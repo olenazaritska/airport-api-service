fa39df30ef3eaa98b570f405a7ad2dfbd3fe340f
Olena Zaritska <zaritskaos@gmail.com>
2024-10-19 13:30:49 +0300

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
