4d493fef6a900713679dff1b1564c0b53dc575a2
Olena Zaritska <zaritskaos@gmail.com>
2024-10-20 14:56:39 +0300

    add throttling config

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -131,6 +131,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    },
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
