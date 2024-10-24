358430aa5fdb522191cc2758bd51a1f30cb6c8bb
Olena Zaritska <zaritskaos@gmail.com>
2024-10-24 12:53:56 +0300

    change access token lifetime

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -169,7 +169,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
